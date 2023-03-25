e568a0ce4d7ba4c471dfa281bb19616d31484132
Haitham Yaghi <125988533+Haitham-Yaghi@users.noreply.github.com>
2023-03-26 03:48:32 +0400

    Update obstacle_avoidance.py

--- src/obstacle_avoidance.py
+++ src/obstacle_avoidance.py
@@ -4,61 +4,67 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
-class obstacle_avoid():
+msg = Twist()
+case = ''
+msg.linear.x = 0
+msg.angular.z = 0
 
+def checkcase(range): 
+   
+    if ( range["right"] > 0.9  and range["center"] < 0.9  and range["left"] < 0.9  ):
+        msg.linear.x =0.3
+        msg.angular.z=0.4
+        pub.publish(msg)
+        case = 'OBSTACLE RIGHT!'
 
-    def __init__(self):
-        self.sub = rospy.Subscriber("/scan", LaserScan , self.callback)
-        self.case = ''
+    elif ( range["right"] < 0.9   and range["center"] < 0.9  and range["left"] > 0.9 ):
+        msg.linear.x=0.3
+        msg.angular.z=-0.4
+        pub.publish(msg)
+        case = 'OBSTACLE LEFT!'
 
-    def checkcase(self,range): 
+    elif ( range["right"] < 0.9   and range["center"] < 0.9  and range["left"] < 0.9  ):
+        msg.linear.x= 0    
+        msg.angular.z=-0.4
+        pub.publish(msg)
+        case = 'OBSTACLE CENTER!'
+       
+    else: 
+        msg.linear.x =0.4
+        msg.angular.z=0
+        pub.publish(msg)
+        case = 'NO OBSTACLE!'
+        
+        
+
+    rospy.loginfo(case)
     
-        if ( range["right"] > 0.9  and range["center"] < 0.9  and range["left"] < 0.9  ):
-            self.case = 'OBSTACLE RIGHT!'
-
-        elif ( range["right"] < 0.9   and range["center"] < 0.9  and range["left"] > 0.9 ):
-            self.case = 'OBSTACLE LEFT!'
-
-        elif ( range["right"] < 0.9   and range["center"] < 0.9  and range["left"] < 0.9  ):
-            self.case = 'OBSTACLE CENTER!'
-        
-        else: 
-            self.case = 'NO OBSTACLE!'
-            
-        
-        rospy.loginfo(self.case)
-        
-    def getCase(self):
-        return self.case
-
-    def callback(self,message):
-        l=len(message.ranges)
-        e = l/3
-        range={
-            "right" : min(min(message.ranges[0:e-1]) , 1),
-            "center" : min(min(message.ranges[e:(2*e-1)]) , 1),
-            "left" : min(min(message.ranges[(2*e):(3*e-1)]) , 1)
-        }
-
-        self.checkcase(range)
-
-    # def listener():
-    #     rospy.init_node('obstacle_avoid', anonymous=False) #This should be in the main node
-    #     #sub = rospy.Subscriber("/scan", LaserScan , callback)
-    #     rospy.spin()
 
 
-class Printer():
-    def __init__(self):
+def callback(message):
+    l=len(message.ranges)
+    e = l/3
+    range={
+        "right" : min(min(message.ranges[0:e-1]) , 1),
+        "center" : min(min(message.ranges[e:(2*e-1)]) , 1),
+        "left" : min(min(message.ranges[(2*e):(3*e-1)]) , 1)
+    }
 
+    checkcase(range)
 
-
-def main():
-    OB=obstacle_avoid()
-    rospy.init_node('obstacle_avoid', anonymous=False) #This should be in the main node
-    rospy.spin()
-
-
+def listener():
+    global pub
+    rospy.init_node('obstacle_avoid', anonymous=False)
+    pub = rospy.Publisher("/cmd_vel" , Twist , queue_size=1)
+    sub = rospy.Subscriber("/scan", LaserScan , callback)
+    msg.linear.x = 0
+    msg.linear.y = 0
+    msg.linear.z = 0
+    msg.angular.x = 0
+    msg.angular.y = 0
+    msg.angular.z = 0
+    
+    rospy.spin()    
 
 if __name__ == '__main__':
-   main()
+    listener()
